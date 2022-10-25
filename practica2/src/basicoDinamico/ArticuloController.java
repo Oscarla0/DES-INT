5873e20cc4bdd65f3280611c773cd83784ebfe38
Oscarla0 <98832763+Oscarla0@users.noreply.github.com>
2022-10-25 19:54:19 +0200

    Add files via upload

---
+++ practica2/src/basicoDinamico/ArticuloController.java
@@ -0,0 +1,64 @@
+package basicoDinamico;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.control.cell.TextFieldTreeCell;
+
+public class ArticuloController {
+
+	 @FXML
+	    private ChoiceBox<String> ChoiceDptTpv;
+
+	    @FXML
+	    private ChoiceBox<String> ChoiceFamilia;
+
+	    @FXML
+	    private ChoiceBox<String> ChoiceGrupos;
+
+	    @FXML
+	    private ChoiceBox<String> ChoiceImptoCompra;
+
+	    @FXML
+	    private ChoiceBox<String> ChoiceImptoVenta;
+
+	    @FXML
+	    private ChoiceBox<String> ChoiceSeccion;
+
+	    @FXML
+	    private ChoiceBox<String> ChoiceUCom;
+
+	    @FXML
+	    private ChoiceBox<String> ChoiceUVenta;
+
+    @FXML
+    private void initialize() {   
+        // Controles de JavaFX a los que se añaden directamente los items 
+    	// Ítems del ChoiceBox
+    	ChoiceDptTpv.getItems().addAll("","Alimentacion", "Bazar", "Bebidas", "Bodega", "Bolleria", 
+        		"Carniceria", "Cerveza");      
+    	ChoiceFamilia.getItems().addAll("","Choice1", "Choice2", "Choice3", "Choice4", "Choice5", 
+        		"Choice6", "Choice7", "Choice8", "Choice9"); 
+    	ChoiceGrupos.getItems().addAll("","Mercaria", "Panaderia y Pasteleria ", "Pescado y Moriscos", "Queso", "Cerrajeria Herrajes", 
+        		"Peleteria", "Completos"); 
+    	ChoiceImptoCompra.getItems().addAll("","4%", "10%", "15%"); 
+    	ChoiceImptoVenta.getItems().addAll("","10%", "15%", "21%"); 
+    	ChoiceSeccion.getItems().addAll("","Aceites", "Alimentos Animales", "Aperitivos", "Azucar y edulcorante", "Cacao", 
+        		"Arroces", "Galletas", "Choice8", "Choice9"); 
+    	ChoiceUCom.getItems().addAll("","1", "2", "3", "4"); 
+    	ChoiceUVenta.getItems().addAll("","1", "2", "3", "4", "5", 
+        		"6", "7", "8"); 
+               
+    }
+    
+}
---
+++ practica2/src/basicoDinamico/Person.java
@@ -0,0 +1,63 @@
+package basicoDinamico;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * 
+ * Clase auxiliar que se empleará para el modelo de datos del TableView
+ * 
+ * @author SBF
+ * 
+ */
+public class Person {
+    private final SimpleStringProperty firstName;
+    private final SimpleStringProperty lastName;
+    private final SimpleStringProperty email;
+    private final SimpleIntegerProperty age;
+    private final SimpleStringProperty familia;
+ 
+    public Person(String fName, String lName, String email, Integer age,String family) {
+        this.firstName = new SimpleStringProperty(fName);
+        this.lastName = new SimpleStringProperty(lName);
+        this.email = new SimpleStringProperty(email);
+        this.age = new SimpleIntegerProperty(age);
+		this.familia = new SimpleStringProperty(family);
+    }
+ 
+    public String getFirstName() {
+        return firstName.get();
+    }
+    
+    public void setFirstName(String fName) {
+        firstName.set(fName);
+    }
+        
+    public String getLastName() {
+        return lastName.get();
+    }
+    public void setLastName(String fName) {
+        lastName.set(fName);
+    }
+    
+    public String getEmail() {
+        return email.get();
+    }
+    public void setEmail(String fName) {
+        email.set(fName);
+    }
+
+	public Integer getAge() {
+		return age.get();
+	}
+	public void setAge(Integer fAge) {
+		age.set(fAge);
+    }        
+        
+    public String getFamilia() {
+		return familia.get();
+	}
+	public void setFamilia(String family) {
+		familia.set(family);
+	}
+}
