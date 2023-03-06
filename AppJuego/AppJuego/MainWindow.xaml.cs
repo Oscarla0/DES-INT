0bb44fea40534824eee9a149096da3f7524d7a31
Oscarla0 <98832763+Oscarla0@users.noreply.github.com>
2023-03-06 23:16:56 +0100

    Add files via upload

---
+++ AppJuego/AppJuego/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppJuego
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 ventana = new Window1();
+
+            this.Visibility = Visibility.Hidden;
+            ventana.ShowDialog();
+
+        }
+
+        private void Button_Click_Inicio(object sender, RoutedEventArgs e)
+        {
+            MainWindow ventana = new MainWindow();
+
+            this.Visibility = Visibility.Hidden;
+            ventana.ShowDialog();
+        }
+    }
+}
---
+++ AppJuego/AppJuego/Window1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppJuego
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 ventana = new Window2();
+            this.Visibility = Visibility.Hidden;
+            ventana.Owner = this;
+            ventana.ShowDialog();
+
+        }
+
+        private void Button_Click_Inicio(object sender, RoutedEventArgs e)
+        {
+            MainWindow ventana = new MainWindow();
+
+            this.Visibility = Visibility.Hidden;
+            ventana.ShowDialog();
+        }
+    }
+}
